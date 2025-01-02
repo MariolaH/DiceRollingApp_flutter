e876af0de4098b5ba806c9fd6073b797b0a9e566
Mariola Hullings <mariolahullings@gmail.com>
2025-01-02 13:44:36 -0500

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+// void-return type // main-func name
+// main() - is the main entry point of a Dart app. It gets executed automatically so need to call it.
+void main() {
+//function - calling or excuting a func
+//runApp(); tells flutter what to display on the screen (ie. whichUI elements to display)
+//runApp() should be called inside of main()
+  runApp();
+}
